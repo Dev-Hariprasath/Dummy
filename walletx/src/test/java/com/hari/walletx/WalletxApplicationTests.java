634596de712096ef684acaa8b922c68c7cbf8e8d
Hariprasath <158441353+Dev-Hariprasath@users.noreply.github.com>
2025-02-03 18:03:04 +0530

    Add files via upload

---
+++ walletx/src/test/java/com/hari/walletx/WalletxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hari.walletx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
